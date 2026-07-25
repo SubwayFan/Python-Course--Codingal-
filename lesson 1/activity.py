41af6c62168e7267b775e9dc7522e5b309323e95
Gabriel <r143guy@gmail.com>
2026-07-24 22:33:11 -0400

    Lesson 1

---
+++ lesson 1/activity.py
@@ -0,0 +1,8 @@
+
+print("Test message")
+
+print("My name is Gabriel.")
+print("I live in the United States.")
+print("One of my favorite things to do outside is to ride trains.")
+print("I enjoy trying to design train stations in Blender.")
+print("I am learning programming.")
